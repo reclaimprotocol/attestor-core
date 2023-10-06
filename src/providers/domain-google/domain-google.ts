3ca941d72a9faefab40a183ab363caea739e50ea
Adhiraj Singh <adhirajsingh1001@gmail.com>
2023-10-06 19:11:49 +0530

    fix: update transcript fetch fns

--- src/providers/domain-google/domain-google.ts
+++ src/providers/domain-google/domain-google.ts
@@ -5,7 +5,7 @@ import { DEFAULT_PORT } from '../../config'
 import { Provider } from '../../types'
 import { uint8ArrayToStr } from '../../utils'
 import {
-	getCompleteHttpResponseFromTranscript,
+	getCompleteHttpResponseFromReceipt,
 	getHttpRequestHeadersFromTranscript,
 } from '../../utils/http-parser'
 
@@ -71,7 +71,7 @@ const googleDomains: Provider<GoogleDomains, GoogleDomainSecretParams> = {
 
 		// parse the HTTP request & check
 		// the method, URL, headers, etc. match what we expect
-		const req = getHttpRequestHeadersFromTranscript(receipt.transcript)
+		const req = getHttpRequestHeadersFromTranscript(receipt)
 		if(req.method !== 'post') {
 			throw new Error(`Invalid method: ${req.method}`)
 		}
@@ -84,7 +84,7 @@ const googleDomains: Provider<GoogleDomains, GoogleDomainSecretParams> = {
 			throw new Error(`Invalid path: ${req.url}`)
 		}
 
-		const res = getCompleteHttpResponseFromTranscript(receipt.transcript)
+		const res = getCompleteHttpResponseFromReceipt(receipt)
 
 		if(res.statusCode !== 200) {
 			throw new Error(`Invalid status code: ${res.statusCode}`)
