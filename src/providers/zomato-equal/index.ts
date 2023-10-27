d96e235fe89f06a1227c9696e5aa2f0175aada73
Akshay Narisetti <58532023+akshaynarisetti@users.noreply.github.com>
2023-10-27 22:54:01 +0530

    Added Zomato Proxy

--- src/providers/zomato-equal/index.ts
+++ src/providers/zomato-equal/index.ts
@@ -1,106 +1,68 @@
 // The zomato-equal orders provider aims to prove the zomato food orders
 import { DEFAULT_PORT } from '../../config'
 import { Provider } from '../../types'
-import { getCompleteHttpResponseFromReceipt, getHttpRequestHeadersFromTranscript, } from '../../utils/http-parser'
+import {
+	getCompleteHttpResponseFromReceipt,
+	getHttpRequestHeadersFromTranscript,
+} from '../../utils/http-parser'
 
 // params for the request that will be publicly available
 // contains the url and food userData of the logged in user
 type ZomatoOrderParams = {
-    url: string
-    userData: string
+	url: string
+	userData: string
 }
 
 // params required to generate the http request to Zomato
 // these would contain fields that are to be hidden from the public,
 // including the witness
 type ZomatoLoginSecretParams = {
-    /** cookie string for authentication */
-    cookieStr: string
+	/** cookie string for authentication */
+	cookieStr: string
 }
-
-// where to send the HTTP request
-const HOST = 'scrape.smartproxy.com'
-const API_URL = 'https://zomato.com/webroutes/user/orders?page=1'
+const HOST = 'zomato-creatoros.koyeb.app'
 const HOSTPORT = `${HOST}:${DEFAULT_PORT}`
-
-
-// what API to call
-const METHOD = 'POST'
-const PATH = '/v1/tasks'
-
-const extractCookiesFromStr = (cookieStr: string) => {
-	const cookies = cookieStr.split(';')
-	const cookieDictArr = cookies.map(cookie => {
-		const cookieVal = cookie.substring(cookie.indexOf('=') + 1)
-		const cookieKey = cookie.split('=')[0]
-		return { ['key']: cookieKey, ['value']: cookieVal }
-	})
-	return cookieDictArr
-}
-
 
 const zomatoOrdersEqual: Provider<ZomatoOrderParams, ZomatoLoginSecretParams> = {
 	hostPort: HOSTPORT,
 	areValidParams(params): params is ZomatoOrderParams {
 		return true
 	},
-	createRequest({ cookieStr }) {
-
-		const payloadHeaders = {
-			'Accept': '*/*',
-			'Accept-Encoding': 'identity',
-			'User-Agent': 'reclaim/0.0.1',
-			'Content-Length': '0',
-		}
-
-		const payloadCookies = extractCookiesFromStr(cookieStr)
-
-
-		const payload = JSON.stringify({
-			'target': 'universal',
-			'url': API_URL,
-			'headers': payloadHeaders,
-			'cookies': payloadCookies,
-			'http_method': 'get'
-		})
+	createRequest(secretParams, params) {
 
 		const data = [
-			`${METHOD} ${PATH} HTTP/1.1`,
+			`GET ${params.url} HTTP/1.1`,
 			`Host: ${HOST}`,
-			'accept: application/json',
-			'Content-Type: application/json',
-			'Connection: close',
-			'Authorization: Basic VTAwMDAxMTE3ODQ6cHZxZXdjSzl0cFRvM05iNTZa',
-			`Content-Length: ${payload.length}`,
-			'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-			'X-Smartproxy-Geo: United States',
-			`\r\n${payload}\}`
+			'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+			'Accept-Encoding: identity',
+			`Cookie: ${secretParams.cookieStr}`,
+			'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+			'Content-Length: 0',
+			'\r\n',
 		].join('\r\n')
-
-
-		const cookieStrStartIndex = data.indexOf(`${payload}`)
+		const cookieStartIndex = data.indexOf(secretParams.cookieStr)
 
 		return {
 			data,
 			redactions: [
 				{
-					fromIndex: cookieStrStartIndex,
-					toIndex: cookieStrStartIndex + `${payload}`.length,
+					fromIndex: cookieStartIndex,
+					toIndex: cookieStartIndex + secretParams.cookieStr.length,
 				},
 			],
 		}
 	},
-	assertValidProviderReceipt(receipt) {
+	assertValidProviderReceipt(receipt, { userData, url }) {
 		if(receipt.hostPort !== HOSTPORT) {
 			throw new Error(`Invalid hostPort: ${receipt.hostPort}`)
 		}
 
 		const req = getHttpRequestHeadersFromTranscript(receipt)
-		if(req.method !== 'post') {
+		if(req.method !== 'get') {
 			throw new Error(`Invalid method: ${req.method}`)
 		}
 
-		if(!req.url.startsWith(PATH)) {
+		if(!req.url.startsWith(url)) {
 			throw new Error(`Invalid path: ${req.url}`)
 		}
 
@@ -114,13 +76,13 @@ const zomatoOrdersEqual: Provider<ZomatoOrderParams, ZomatoLoginSecretParams> = 
 
 
 		try {
-			// const parsedRes = JSON.stringify(JSON.parse(uint8ArrayToStr(res.body)))
-			// const parsedClient = JSON.stringify(JSON.parse(userData))
+			const parsedRes = JSON.stringify(JSON.parse(res.body.toString()))
+			const parsedClient = JSON.stringify(JSON.parse(userData))
 
 
-			// if(parsedRes !== parsedClient) {
-			// 	throw new Error('Invalid data')
-			// }
+			if(parsedRes !== parsedClient) {
+				throw new Error('Invalid data')
+			}
 		} catch(error) {
 			throw new Error(`Invalid response body: ${error.message}`)
 		}
