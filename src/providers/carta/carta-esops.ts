68fdaa2990474373a33c2dfa5f95abf271757588
Kushal7788 <kushshah777888@gmail.com>
2023-11-16 11:52:58 +0300

    Added CsrfToken in request call

--- src/providers/carta/carta-esops.ts
+++ src/providers/carta/carta-esops.ts
@@ -1,47 +1,113 @@
-import { wrapInHttpProvider } from '../http-provider/wrapper'
+/**
+ * Use the Carta API to get the username of the user
+ * to prove they are owners of the same usernname.
+ *
+ * https://www.carta.so/api/v3/getSpaces
+ */
 
-// params for the request that will be publicly available
-// contains the username of the logged in user and x-csrf-token
-type CartaEsops = {
+import { DEFAULT_PORT } from '../../config'
+import { Provider } from '../../types'
+import { uint8ArrayToStr } from '../../utils'
+import { getCompleteHttpResponseFromReceipt, getHttpRequestHeadersFromTranscript } from '../../utils/http-parser'
+
+type CartaUsernameParams = {
 	count: number
+}
+
+type CartaUsernameSecretParams = {
 	userId: string
-}
-
-// params required to generate the http request to Kaggle
-// these would contain fields that are to be hidden from the public,
-// including the witness
-type CartaSecretParams = {
-	/** cookie string for authentication */
 	cookieStr: string
+	csrfToken: string
 }
 
-const cartaEsopsCount = wrapInHttpProvider({
-	getParams: ({ count, userId }: CartaEsops) => (
-		{
-			headers: {
-				'accept': 'application/json, text/plain, */*',
-				'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8'
-			},
-			url: `https://app.carta.com/api/investors/portfolio/fund/${userId}/list/`,
-			method: 'GET',
-			responseSelections: [
+// where to send the HTTP request
+const HOST = 'app.carta.com'
+const HOSTPORT = `${HOST}:${DEFAULT_PORT}`
+
+// what API to call
+const METHOD = 'GET'
+
+
+// Define the provider
+const cartaEsopsCount: Provider<CartaUsernameParams, CartaUsernameSecretParams> = {
+	hostPort: HOSTPORT,
+	areValidParams(params): params is CartaUsernameParams {
+		return typeof params.count === 'number'
+	},
+	createRequest({ userId, cookieStr, csrfToken }) {
+		// serialise the HTTP request
+		const url = `/api/investors/portfolio/fund/${userId}/list/`
+		const data = [
+			`${METHOD} ${url} HTTP/1.1`,
+			'Host: ' + HOST,
+			`cookie: ${cookieStr}`,
+			'accept: application/json, text/plain, */*',
+			'accept-language: en-GB,en-US;q=0.9,en;q=0.8',
+			'Connection: close',
+			`referer: https://app.carta.com/investors/individual/${userId}/portfolio/`,
+			`x-csrftoken: ${csrfToken}`,
+			'\r\n'
+		].join('\r\n')
+
+		// find the token and redact it
+		const tokenStartIndex = data.indexOf(cookieStr)
+
+		return {
+			data,
+			// anything that should be redacted from the transcript
+			// should be added to this array
+			redactions: [
 				{
-					'jsonPath': '$.count',
-					'responseMatch': `\"count\":${count}`
+					fromIndex: tokenStartIndex,
+					toIndex: tokenStartIndex + cookieStr.length
 				}
 			]
 		}
-	),
-	getSecretParams: ({ cookieStr }: CartaSecretParams) => ({
-		cookieStr: cookieStr
-	}),
-	areValidParams: (params): params is CartaEsops => {
-		return (
-			typeof params.count === 'number' && typeof params.userId === 'string'
-		)
-	}
-})
+	},
+	assertValidProviderReceipt(receipt, { count }) {
+		// ensure the request was sent to the right place
+		if(receipt.hostPort !== HOSTPORT) {
+			throw new Error(`Invalid hostPort: ${receipt.hostPort}`)
+		}
+
+		// parse the HTTP request & check
+		// the method, URL, headers, etc. match what we expect
+		const req = getHttpRequestHeadersFromTranscript(receipt)
+		if(req.method !== METHOD.toLowerCase()) {
+			throw new Error(`Invalid method: ${req.method}`)
+		}
+
+		// we ensure the connection header was sent as "close"
+		// this is done to avoid any possible malicious request
+		// that contains multiple requests, but via redactions
+		// is spoofed as a single request
+		if(req.headers['connection'] !== 'close') {
+			throw new Error('Invalid connection header')
+		}
+
+		// now we parse the HTTP response & check
+		// if the userName returned by the API
+		// matches the parameters the user provided
+		const res = getCompleteHttpResponseFromReceipt(receipt)
+		if(res.statusCode !== 200) {
+			throw new Error(`Invalid status code: ${res.statusCode}`)
+		}
+
+		if(!res.headers['content-type']?.startsWith('application/json')) {
+			throw new Error(`Invalid content-type: ${res.headers['content-type']}`)
+		}
+
+		// parse the body & try to find the username
+		// inside the JSON response from the API
+		const json = JSON.parse(uint8ArrayToStr(res.body))
+		if(json?.count === undefined) {
+			throw new Error(`Invalid JSON response: ${json}`)
+		}
+
+		if(count !== json.count) {
+			throw new Error(`Count "${json.count}" does not match expected "${count}"`)
+		}
+	},
+}
 
 export default cartaEsopsCount
-
-
