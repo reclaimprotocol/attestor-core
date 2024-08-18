92338ad79437973343e59697859cc7ebc0feeb3e
Alex E <alex@creatoros.co>
2024-08-18 23:04:07 +0500

    [FIX] remove log

--- src/utils/claims.ts
+++ src/utils/claims.ts
@@ -104,7 +104,6 @@ export function getIdentifierFromClaimInfo(info: ClaimInfo): ClaimID {
 	}
 
 	const str = `${info.provider}\n${info.parameters}\n${info.context || ''}`
-	console.log('Identifier: ' + Buffer.from(str).toString('base64'))
 	return utils.keccak256(
 		strToUint8Array(str)
 	).toLowerCase()
