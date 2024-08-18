47722721def458adf6ac1f9a7c7c6dd01abf6e5e
Alex E <alex@creatoros.co>
2024-08-18 18:55:39 +0500

    [CHORE] log identifier string

--- src/utils/claims.ts
+++ src/utils/claims.ts
@@ -104,6 +104,7 @@ export function getIdentifierFromClaimInfo(info: ClaimInfo): ClaimID {
 	}
 
 	const str = `${info.provider}\n${info.parameters}\n${info.context || ''}`
+	console.log('Identifier: ' + str)
 	return utils.keccak256(
 		strToUint8Array(str)
 	).toLowerCase()
