635a982ab5a2ed138272e6bc2a1b3e5370cc005c
Adhiraj Singh <adhirajsingh1001@gmail.com>
2024-10-05 11:31:54 +0530

    feat: whitelist operator script

---
+++ src/scripts/whitelist-operator.ts
@@ -0,0 +1,22 @@
+// eslint-disable-next-line simple-import-sort/imports
+import 'src/server/utils/config-env'
+import { getContracts } from 'src/avs/utils/contracts'
+import { getCliArgument } from 'src/scripts/utils'
+
+async function main() {
+	const { contract } = getContracts()
+
+	const address = getCliArgument('address')
+	if(!address) {
+		throw new Error(
+			'Provide operator address via --address <addr>'
+		)
+	}
+
+	const tx = await contract.whitelistAddressAsOperator(address, true)
+	await tx.wait()
+
+	console.log('Whitelisted address:', address)
+}
+
+main()
