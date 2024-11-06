fe0e4b17e90e3d069758b358eaf5ce2e9b5d9dd8
Adhiraj Singh <adhirajsingh1001@gmail.com>
2024-11-06 12:33:18 +0530

    feat: add avs metadata update script

---
+++ src/scripts/update-avs-metadata.ts
@@ -0,0 +1,26 @@
+// eslint-disable-next-line simple-import-sort/imports
+import 'src/server/utils/config-env'
+import { getContracts } from 'src/avs/utils/contracts'
+import { getCliArgument } from 'src/scripts/utils'
+
+async function main() {
+	const { contract } = getContracts()
+
+	const minSignaturesPerTask = getCliArgument('minSignaturesPerTask')
+	if(!minSignaturesPerTask) {
+		throw new Error(
+			'Provide operator address via --minSignaturesPerTask <num>'
+		)
+	}
+
+	const tx = await contract.updateTaskCreationMetadata({
+		minSignaturesPerTask: +(minSignaturesPerTask || 0),
+		maxTaskCreationDelayS: 0,
+		maxTaskLifetimeS: 0,
+	})
+	await tx.wait()
+
+	console.log('Updated task creation metadata')
+}
+
+main()
