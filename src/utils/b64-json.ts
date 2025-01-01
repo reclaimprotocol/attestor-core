cefbea03d7113bef310094f3db04f034bfd6bcc1
Adhiraj Singh <adhirajsingh1001@gmail.com>
2025-01-01 11:38:17 +0530

    fix: add b64 json reviver

--- src/utils/b64-json.ts
+++ src/utils/b64-json.ts
@@ -1,8 +1,24 @@
 import { base64 } from 'ethers/lib/utils'
 
 export const B64_JSON_REPLACER = (key: string, value: any) => {
-	if(value instanceof Uint8Array) {
-		return base64.encode(value)
+	if(
+		value instanceof Uint8Array
+		|| (
+			typeof value === 'object'
+			&& value
+			&& 'buffer' in value
+			&& value.buffer instanceof ArrayBuffer
+		)
+	) {
+		return { type: 'uint8array', value: base64.encode(value) }
+	}
+
+	return value
+}
+
+export const B64_JSON_REVIVER = (key: string, value: any) => {
+	if(value?.type === 'uint8array') {
+		return base64.decode(value.value)
 	}
 
 	return value
--- src/window-rpc/setup-window-rpc.ts
+++ src/window-rpc/setup-window-rpc.ts
@@ -5,7 +5,7 @@ import { createClaimOnAttestor } from 'src/client'
 import { extractHTMLElement, extractJSONValueIndex, generateRequstAndResponseFromTranscript } from 'src/providers/http/utils'
 import { OPRFOperators, ProviderParams, ProviderSecretParams, ZKOperators } from 'src/types'
 import { makeLogger } from 'src/utils'
-import { B64_JSON_REPLACER } from 'src/utils/b64-json'
+import { B64_JSON_REPLACER, B64_JSON_REVIVER } from 'src/utils/b64-json'
 import { Benchmark } from 'src/utils/benchmark'
 import { CommunicationBridge, RPCCreateClaimOptions, WindowRPCClient, WindowRPCErrorResponse, WindowRPCIncomingMsg, WindowRPCOutgoingMsg, WindowRPCResponse } from 'src/window-rpc/types'
 import { generateRpcRequestId, getCurrentMemoryUsage, getWsApiUrlFromLocation, mapToCreateClaimResponse, waitForResponse } from 'src/window-rpc/utils'
@@ -46,7 +46,7 @@ export function setupWindowRpc() {
 
 			const req: WindowRPCIncomingMsg = (
 				typeof event.data === 'string'
-					? JSON.parse(event.data)
+					? JSON.parse(event.data, B64_JSON_REVIVER)
 					: event.data
 			)
 
