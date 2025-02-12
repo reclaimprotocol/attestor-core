9f48fe776ebd453a0d64c69929006adb5795912f
Adhiraj Singh <adhirajsingh1001@gmail.com>
2025-02-12 18:02:32 +0530

    feat: trace http reqs

--- src/scripts/start-server.ts
+++ src/scripts/start-server.ts
@@ -1,4 +1,12 @@
-import { createServer } from 'src/server'
+import { getApm } from 'src/server/utils/apm'
 import 'src/server/utils/config-env'
+getApm()
 
-void createServer()
+function main() {
+	// importing dynamically to allow APM to inject
+	// into modules before they are used
+	const { createServer } = require('../server/create-server')
+	return createServer()
+}
+
+main()
