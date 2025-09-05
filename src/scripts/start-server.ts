0fd2a80b15917c96e70cc01281d87139aa5a8090
Adhiraj Singh <adhirajsingh1001@gmail.com>
2025-09-06 04:58:52 +0530

    fix: start cmd crypto

--- src/scripts/start-server.ts
+++ src/scripts/start-server.ts
@@ -1,7 +1,11 @@
+import { setCryptoImplementation } from '@reclaimprotocol/tls'
+import { webcryptoCrypto } from '@reclaimprotocol/tls/webcrypto'
 import '#src/server/utils/config-env.ts'
 
 import { getApm } from '#src/server/utils/apm.ts'
 getApm()
+
+setCryptoImplementation(webcryptoCrypto)
 
 async function main() {
 	// importing dynamically to allow APM to inject
