25d2ddd3b2d4a4458bcdd87c646549764b155a3b
Alex E <alex@reclaimprotocol.org>
2025-09-10 17:55:05 +0500

    [WIP] init all 3 OPRF circuits

--- src/server/handlers/claimTeeBundle.ts
+++ src/server/handlers/claimTeeBundle.ts
@@ -17,7 +17,7 @@ import { verifyTeeBundle } from '#src/server/utils/tee-verification.ts'
 import type { Logger } from '#src/types/general.ts'
 import type { ProviderCtx, RPCHandler, Transcript } from '#src/types/index.ts'
 import { AttestorError } from '#src/utils/error.ts'
-import { createSignDataForClaim, getIdentifierFromClaimInfo, uint8ArrayToStr } from '#src/utils/index.ts'
+import { createSignDataForClaim, getIdentifierFromClaimInfo } from '#src/utils/index.ts'
 
 export const claimTeeBundle: RPCHandler<'claimTeeBundle'> = async(
 	teeBundleRequest,
@@ -67,7 +67,6 @@ export const claimTeeBundle: RPCHandler<'claimTeeBundle'> = async(
 						oprfResults,
 						logger
 					)
-					console.log(uint8ArrayToStr(message.message))
 				}
 			}
 		}
