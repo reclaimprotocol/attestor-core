182fa03e292b48d4cf8ce3aa9b141b1be8fba196
Alex E <alex@reclaimprotocol.org>
2025-09-24 10:16:52 +0500

    [WIP] revert oprf

--- src/server/handlers/toprf.ts
+++ src/server/handlers/toprf.ts
@@ -18,12 +18,8 @@ export const toprf: RPCHandler<'toprf'> = async(
 	const PUBLIC_KEY = ethers.utils.arrayify(PUBLIC_KEY_STR)
 
 	const engineStr = getEngineString(engine)
-	//init all algorithms
-	const operator1 = makeDefaultOPRFOperator('chacha20', engineStr, logger)
-	const operator2 = makeDefaultOPRFOperator('aes-128-ctr', engineStr, logger)
-	const operator3 = makeDefaultOPRFOperator('aes-256-ctr', engineStr, logger)
-	await operator1.evaluateOPRF(PRIVATE_KEY, maskedData, logger)
-	await operator2.evaluateOPRF(PRIVATE_KEY, maskedData, logger)
-	const res = await operator3.evaluateOPRF(PRIVATE_KEY, maskedData, logger)
+	const operator = makeDefaultOPRFOperator('chacha20', engineStr, logger)
+	const res = await operator.evaluateOPRF(PRIVATE_KEY, maskedData)
+
 	return { ...res, publicKeyShare: PUBLIC_KEY }
 }
