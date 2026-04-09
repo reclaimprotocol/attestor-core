04cdd05b2bb89230c4789037f8375a24f09fcec8
Adhiraj Singh <adhirajsingh1001@gmail.com>
2026-04-09 22:07:26 +0700

    fix: tests

--- src/tests/http-provider.test.ts
+++ src/tests/http-provider.test.ts
@@ -45,7 +45,7 @@ describeWithServer('HTTP Provider', opts => {
 		assert.ok(!resp.error)
 		assert.match(
 			resp.claim?.context || '',
-			/0x5e3e976476ded7b58120d606b33b75be52adb8345a7979c181764f00763e7b2a/
+			/0x9b26b1c18a7169a7533d4b6d1969198db8df8be52f58aad6d729ca3ecf90ea17/
 		)
 	})
 
