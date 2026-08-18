7c731d8419b8a978ceb3061067ffddd64a6e4ab0
Mushaheed Syed <mushaheed.syed@proton.me>
2026-08-18 07:25:58 -0700

    Merge pull request #92 from reclaimprotocol/export-extract
    Add extract json path and x path to public exports

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,6 +1,15 @@
 import http from '#src/providers/http/index.ts'
 import type { Provider, ProviderName } from '#src/types/index.ts'
 
+export {
+	extractHTMLElement,
+	extractHTMLElementIndex,
+	extractHTMLElements,
+	extractHTMLElementsIndexes,
+	extractJSONValueIndex,
+	extractJSONValueIndexes,
+} from '#src/providers/http/utils.ts'
+
 export const providers: {
 	[T in ProviderName]: Provider<T>
 } = {
